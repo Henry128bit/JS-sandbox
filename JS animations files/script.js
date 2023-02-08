620ab90a548598f51548ded6af79df10019db540
Sergey Kamensky <henry.128bit@gmail.com>
2023-02-08 15:51:59 +0400

    Work with arrays

--- JS animations files/script.js
+++ JS animations files/script.js
@@ -1,23 +1,135 @@
-console.log('Hello World!');
+// console.log('Hello World!');
 
-const btn = document.querySelector('.btn');
+// const btn = document.querySelector('.btn');
 
-function myAnimation() {
-    const element = document.querySelector('.box'),
-          id = setInterval(frame, 10);
-    let position = 0;
+// function myAnimation() {
+//     const element = document.querySelector('.box'),
+//           id = setInterval(frame, 10);
+//     let position = 0;
 
-    function frame() {
-        if (position == 300) {
-            clearInterval(id);
-        } else {
-            position++;
-            element.style.top = position +  'px';
-            element.style.left = position + 'px';
-        }
+//     function frame() {
+//         if (position == 300) {
+//             clearInterval(id);
+//         } else {
+//             position++;
+//             element.style.top = position +  'px';
+//             element.style.left = position + 'px';
+//         }
+//     }
+
+//     btn.addEventListener('click', myAnimation);
+// }
+
+// btn.addEventListener('click', myAnimation);
+
+// const now = new Date();
+// console.log(now.setHours(18));
+// console.log(now);
+
+// Задачи с переборами массивов
+
+const films = [
+    {
+        name: 'Titanic',
+        rating: 9
+    },
+    {
+        name: 'Die hard 5',
+        rating: 5
+    },
+    {
+        name: 'Matrix',
+        rating: 8
+    },
+    {
+        name: 'Some bad film',
+        rating: 4
     }
+];
 
-    btn.addEventListener('click', myAnimation);
+// 1) У вас есть список фильмов с рейтингом в виде массива объектов. 
+// Напишите функцию showGoodFilms, которая будет принимать этот массив, а возвращать будет массив объектов только с теми 
+// фильмами, у которых рейтинг больше или равен 8.
+
+function showGoodFilms(arr) {
+    return arr.filter(item => item.rating >= 8);
 }
 
-btn.addEventListener('click', myAnimation);
+console.log(showGoodFilms(films));
+
+// 2) Напишите функцию showListOfFilms, которая будет принимать этот же массив, а возвращать будет строку, которая содержит 
+// названия фильмов через запятую.
+
+function showListOfFilms(arr) {
+    return arr.map(item => item.name)
+           .reduce((sum, current) => `${sum}, ${current}`);
+}
+
+console.log(showListOfFilms(films));
+
+// 3) Напишите функцию setFilmsIds, которая будет принимать этот же массив, а возвращать будет такой же массив с фильмами, но 
+// у каждого фильма будет новое поле id. Значение этого поля установите по нумерации фильма.
+
+function setFilmsIds(arr) {
+    let i = 0;
+    return arr.map(function(item){
+        item.id = i;
+        i++;
+        return item;
+    });
+}
+
+console.log(setFilmsIds(films));
+
+// 4) Запишите результат предыдущей функции в переменную tranformedArray. Напишите функцию checkFilms, которая будет проверять, 
+// что в каждом из фильмов есть поле id. Если это так - функция возвращает true. Очевидно, что сейчас условие должно выполняться, 
+// если мы передаем checkFilms(tranformedArray);
+
+const tranformedArray = setFilmsIds(films);
+
+function checkFilms(arr) {
+    let key = 'id';
+    return arr.every(item => Object.prototype.hasOwnProperty.call(item, key) === true);
+}
+
+console.log(checkFilms(tranformedArray));
+
+// Задачи с переборами массивов
+
+const funds = [
+    {amount: -1400},
+    {amount: 2400},
+    {amount: -1000},
+    {amount: 500},
+    {amount: 10400},
+    {amount: -11400}
+];
+
+// 1) У вас есть небольшой массив с данными о доходах каждой торговой точки. Напишите функцию getPositiveIncomeAmount, которая 
+// принимает этот массив данных и возвращает сумму только положительных значений из каждого объекта. (число)
+
+const getPositiveIncomeAmount = (data) => {
+    return data.map(item => item.amount)
+               .filter(amount => amount > 0)
+               .reduce((sum, current) => sum + current);
+};
+
+console.log(getPositiveIncomeAmount(funds));
+
+// 2) Напишите функцию getTotalIncomeAmount, которая тоже принимает этот массив данных. Если хотя бы один из объектов содержит 
+// отрицательное значение поля amount, то функция возвращает сумму всех значений. (число) Если таких значений нет - запускается 
+// функция getPositiveIncomeAmount с тем же массивом данных.
+
+// Пример:
+// getTotalIncomeAmount(funds) => -500
+
+const getTotalIncomeAmount = (data) => {
+    if (data.some(item => item.amount < 0)) {
+        return data.map(item => item.amount)
+                   .reduce((sum, current) => sum + current);
+    } else {
+        getPositiveIncomeAmount(data);
+    }
+};
+
+console.log(getTotalIncomeAmount(funds));
