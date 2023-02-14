3cf9542ecbef15e4afa1facddf0567f87c8c7f9c
Sergey Kamensky <henry.128bit@gmail.com>
2023-02-14 16:36:37 +0400

    OOP

--- JS animations files/script.js
+++ JS animations files/script.js
@@ -133,3 +133,34 @@ const getTotalIncomeAmount = (data) => {
 };
 
 console.log(getTotalIncomeAmount(funds));
+
+// Принципы ООП
+
+// 1.Инкапсуляция 
+
+class User {
+    constructor(name, age) {
+        this.name = name;
+        this._age = age;
+    }
+
+    #surname = 'Petrychenko';
+
+    say = () => {
+        console.log(`Имя пользователя: ${this.name} ${this.#surname}, возраст ${this._age}`);
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(age) {
+        if (typeof age === 'number' && age > 0 && age < 110) {
+            this._age = age;
+        } else {
+            console.log('Недопустимое значение!');
+        }
+    }
+}
+
+const ivan = new User('Ivan', 27);
